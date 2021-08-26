0e663239fee28081a6e487dbe679fcd83fdd9373
Biraj Silwal <birajsilwal.ai@gmail.com>
2021-08-26 16:23:26 -0600

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/Home.css";
-import Banner from "../components/Banner";
 import NewBanner from "../components/NewBanner";
 import Experience from "../components/Experience";
 import Portfolio from "../components/Portfolio";
-import AboutMe from "../components/AboutMe";
 import GithubStat from "../components/GithubStat";
 
 export default function Home() {
